27f49b0c90caa07b251341466c9f1b31c4c4a74f
Steven Yung <stevenyung@icloud.com>
2017-10-08 12:33:21 +0200

    implement minimal version of the package and fixed minimum laravel version to 55

--- src/AutoRouteServiceProvider.php
+++ src/AutoRouteServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace Expendables\AutoRoute;
 
+use ReflectionClass;
+use Illuminate\Routing\{Router, Route};
 use Illuminate\Support\ServiceProvider;
 
 class AutoRouteServiceProvider extends ServiceProvider
@@ -13,7 +15,34 @@ class AutoRouteServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Router::macro('getUnnamedRoutes', function () {
+            return array_filter($this->getRoutes()->getRoutes(), function ($route) {
+                return ($route->getName() === null);
+            });
+        });
+
+        Route::macro('getControllerName', function () {
+            $controllerName = (new ReflectionClass($this->getController()))->getShortName();
+
+            preg_match('/(\w+)Controller/', $controllerName, $matches);
+
+            return strtolower($matches[1]);
+        });
+
+        Router::macro('refreshDefaultNames', function () {
+            foreach ($this->getUnnamedRoutes() as $route)
+            {
+                $name = $route->getControllerName();
+
+                $method = $route->getActionMethod();
+
+                $route->name("{$name}.{$method}");
+            }
+        });
+
+        $this->app->booted(function () {
+            $this->app['router']->refreshDefaultNames();
+        });
     }
 
     /**
