829e4dc68ecdae2a97d695622c2c6b40c9d9fbfe
Steven Yung <stevenyung@icloud.com>
2017-10-08 11:52:07 +0200

    add AutoRouteServiceProvider

---
+++ src/Providers/AutoRouteServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Expendables\AutoRoute\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class AutoRouteServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
