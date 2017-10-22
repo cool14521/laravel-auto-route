a61f970fab4036c25fcd0e52b24aad3c9010bd6f
Steven Yung <stevenyung@icloud.com>
2017-10-22 15:16:09 +0200

    setup testing environment with Orchestra

---
+++ tests/RouteTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Expendables\AutoRoute\Tests\Unit;
+
+use Expendables\AutoRoute\Tests;
+use Orchestra\Testbench\TestCase;
+
+class RouteTest extends TestCase
+{
+    /** @test */
+    public function example()
+    {
+        $this->assertTrue(true);
+    }
+}
