5dcb84d59bf2e54babcd4253fc971e13b0ca27d2
Steven Yung <stevenyung@icloud.com>
2017-10-22 18:01:30 +0200

    implement RouteTest@setUp

--- tests/RouteTest.php
+++ tests/RouteTest.php
@@ -6,26 +6,33 @@ use Expendables\AutoRoute\Tests\Stubs\TestsController;
 
 class RouteTest extends TestCase
 {
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $this->controller = TestsController::class;
+        $this->router->get('/', "{$this->controller}@index");
+        $this->route = $this->app['router']->getRoutes()->getRoutes()[0];
+    }
+
     /** @test */
     public function can_get_controller_name()
     {
-        $controller = TestsController::class;
-        $this->router->get('/', "{$controller}@index");
-        $route = $this->app['router']->getRoutes()->getRoutes()[0];
-
-        $res = $route->getControllerName();
+        $res = $this->route->getControllerName();
 
         $this->assertEquals('tests', $res);
     }
 
     /** @test */
+    public function cannot_pass_controller_class_without_trailing_controller_word()
+    {
+        //
+    }
+
+    /** @test */
     public function can_get_controller_short_name()
     {
-        $controller = TestsController::class;
-        $this->router->get('/', "{$controller}@index");
-        $route = $this->router->getRoutes()->getRoutes()[0];
-
-        $result = $route->getControllerShortName();
+        $result = $this->route->getControllerShortName();
 
         $this->assertEquals('TestsController', $result);
     }
