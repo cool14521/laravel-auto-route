edd57ebeef41ecb841a3bf783cd5c37a1d2766ae
Steven Yung <stevenyung@icloud.com>
2017-11-10 01:09:37 +0100

    implement config/autoroute.php

--- src/AutoRouteServiceProvider.php
+++ src/AutoRouteServiceProvider.php
@@ -5,6 +5,9 @@ namespace Expendables\AutoRoute;
 use ReflectionClass;
 use Illuminate\Routing\{Router, Route};
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Contracts\Container\Container;
+use Laravel\Lumen\Application as LumenApplication;
+use Illuminate\Foundation\Application as LaravelApplication;
 
 class AutoRouteServiceProvider extends ServiceProvider
 {
@@ -15,7 +18,18 @@ class AutoRouteServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        $source = realpath($raw = __DIR__.'/../config/autoroute.php') ?: $raw;
+
+        if ($this->app instanceof LaravelApplication && $this->app->runningInConsole()) {
+            $this->publishes([$source => config_path('autoroute.php')]);
+        } elseif ($this->app instanceof LumenApplication) {
+            $this->app->configure('autoroute');
+        }
+
+        $this->mergeConfigFrom($source, 'autoroute');
+
         Router::mixin(new RouterMixin);
+
         Route::mixin(new RouteMixin);
 
         $this->app->booted(function () {
